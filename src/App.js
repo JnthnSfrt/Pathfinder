9ee1bb5b3611b9f998a9bdc667bb1ad313662939
ajtrs <jonathanseifert1998@gmail.com>
2021-09-15 12:39:23 +0200

    v0.1.2

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="Pathfinder">
       <header className="App-header">
-        Pathfinder v0.1.1
+        Pathfinder v0.1.2
       </header>
       <Pathfinder></Pathfinder>
     </div>
