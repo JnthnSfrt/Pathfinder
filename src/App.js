47638c30c6c6c13519637752f9756d274da4bfd7
ajtrs <jonathanseifert1998@gmail.com>
2021-09-17 00:23:48 +0200

    Update version

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="Pathfinder">
       <header className="App-header">
-        Pathfinder v0.1.3
+        Pathfinder v0.1.4
       </header>
       <Pathfinder></Pathfinder>
     </div>
