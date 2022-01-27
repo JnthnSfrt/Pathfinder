8df082033a1963344be357fc71c452f0f94b9f5d
Jonathan Seifert <jonathanseifert1998@gmail.com>
2022-01-27 10:59:37 +0100

    Layout and CSS rework

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import './Styles/App.css';
 import './Styles/hyperlinks.css'
 import './Styles/slider.css';
+import './Styles/hamburger-menu.css';
 import react, { useState, useEffect } from 'react';
-import Pathfinder from './Pathfinder/Pathfinder';
+import Pathfinder from './Pathfinder/Pathfinder.jsx';
 
 function getCurrentScreenSize() {
   let width = window.innerWidth;
@@ -20,26 +21,34 @@ function App() {
   }, [darkMode]);
 
   return (
-    <body>
-      <div className="App" data-theme={darkMode ? "light" : "dark"}>
-        <header className="App-header">
-          <div id="empty"></div>
-          <div id="title">Pathfinder Version 0.6.2</div>
-          <div id="slider">
-            <label class="switch">
-              <input type="checkbox" onClick={toggleDarkMode}></input>
-              <span class="slider round"></span>
-            </label>
-          </div>
-        </header>
-        <div class="content">
-          <Pathfinder />
+    <body data-theme={darkMode ? "light" : "dark"}>
+      <div className="header">
+        Pathfinder v0.6.2
+        {/* Hamburger-Menü */}
+        <div className="hamburger-menu">
+          <input id="menu__toggle" type="checkbox" />
+          <label class="menu__btn" for="menu__toggle">
+            <span></span>
+          </label>
+          <ul class="menu__box">
+            {/* Darkmode-Slider */}
+            <div id="slider">
+              <label class="switch">
+                <input type="checkbox" onClick={toggleDarkMode}></input>
+                <span class="slider round"></span>
+              </label>
+            </div>
+            <button class="menu__item" onClick=
+              {() => this.setStartNode()}>Set start node</button>
+            <button class="menu__item">ist</button>
+            <button class="menu__item">ein</button>
+            <button class="menu__item">Test.</button>
+            Dimension: <span id="dimension">{window.innerWidth} * {window.innerHeight}</span>
+          </ul>
         </div>
-        <footer class="footer">
-          <div id="github-link">
-            <a href="https://github.com/JonathanSeifert/Pathfinder" target="_blank">github.com/JonathanSeifert/Pathfinder</a>
-          </div>
-        </footer>
+      </div>
+      <div className="content">
+        <Pathfinder />
       </div>
     </body>
   );
